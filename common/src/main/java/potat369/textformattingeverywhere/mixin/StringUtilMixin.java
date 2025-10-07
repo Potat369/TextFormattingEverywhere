3e60a73a148665752fc35efc6f1663abc4203789
Potat369 <yevheniidemian@gmail.com>
2025-10-07 12:19:47 +0300

    Actually fix #5

--- common/src/main/java/potat369/textformattingeverywhere/mixin/StringUtilMixin.java
+++ common/src/main/java/potat369/textformattingeverywhere/mixin/StringUtilMixin.java
@@ -8,15 +8,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(StringUtil.class)
 public class StringUtilMixin {
-    // Prior to 1.20.9
     @Inject(method = "isAllowedChatCharacter(C)Z", at = @At("RETURN"), cancellable = true)
     private static void isAllowedChatCharacter(char character, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(character != 167 ? cir.getReturnValue() : true);
     }
 
-    // After 1.20.9
     @Inject(method = "method_57175(I)Z", remap = false, at = @At("RETURN"), cancellable = true)
-    private static void isAllowedChatCharacterInt(int character, CallbackInfoReturnable<Boolean> cir) {
+    private static void isAllowedChatCharacterFabric(int character, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(character != 167 ? cir.getReturnValue() : true);
+    }
+
+    @Inject(method = "isAllowedChatCharacter(I)Z", remap = false, at = @At("RETURN"), cancellable = true)
+    private static void isAllowedChatCharacterForge(int character, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(character != 167 ? cir.getReturnValue() : true);
     }
 }
